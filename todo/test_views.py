d870d6b91e6e34be6cfb4222fca64791bb6ddfc6
jamesigibbs <jamesigibbs180@gmail.com>
2021-02-01 18:57:12 +0000

    completed model testing and fixed views tests

--- todo/test_views.py
+++ todo/test_views.py
@@ -32,8 +32,15 @@ class TestViews(TestCase):
         self.assertEqual(len(existing_item), 0)
 
     def test_can_toggle_item(self):
-        item = Item.objects.create(name='Test Todo Item')
+        item = Item.objects.create(name='Test Todo Item', done=True)
         response = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+
+    def test_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
