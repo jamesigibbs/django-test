fa9b690e13796c53c64b7173585c815934dc0572
jamesigibbs <jamesigibbs180@gmail.com>
2021-02-01 19:53:58 +0000

    Added heroku to allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '+c%-opipstt8=$7spskbypy6fi+mnt9+%z6&x0add-j1-iabr2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['james-django-todo-app.herokuapp.com']
 
 
 # Application definition
